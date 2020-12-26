d52085c68e6c6bd1ac7546ddc0440b1111c9db10
LowDensity <hailhailHaily@hotmail.com>
2020-12-26 18:25:15 +0800

    重新架構FictionalTime.js

--- FictionalTime.js
+++ FictionalTime.js
@@ -1,70 +1,58 @@
 //設計中的Fictional Time
 
 //prototype for calendar class
-var time_unit={
-    "schema" :{    
-        "order" :[
-            "year"
-            ,"day"
-            ,"hour"
-            ,"minute"
-            ,"second"
-        ]
-    },
-    "second":{
-        "volume" : 1000,
-        "volume_type" : "accumulate",
-        "unit_type": "AM" // AM = Absolute Minimum   
-    },
-    "minute":{
-        "volume" : 60,
-        "volume_type" : "accumulate",
-        "unit_type":"second"
-    },
-    "hour":{
-        "volume" :  60 ,
-        "volume_type" : "accumulate",
-        "unit_type":"minute",
-    },
-    "day":{
-        "volume" :17,
-        "volume_type" : "accumulate",
-        "unit_type" :"hour"
-    },
-    "year":{
-        "volume" :553,
-        "volume_type" : "periodically",
-        "unit_type" :"day"
+// var time_unit={
+
+//     "second":{
+//         "volume" : 1000,
+//         "volume_type" : "accumulate",
+//         "unit_type": "AM" // AM = Absolute Minimum   
+//     },
+//     "minute":{
+//         "volume" : 60,
+//         "volume_type" : "accumulate",
+//         "unit_type":"second"
+//     },
+//     "hour":{
+//         "volume" :  60 ,
+//         "volume_type" : "accumulate",
+//         "unit_type":"minute",
+//     },
+//     "day":{
+//         "volume" :17,
+//         "volume_type" : "accumulate",
+//         "unit_type" :"hour"
+//     },
+//     "year":{
+//         "volume" :553,
+//         "volume_type" : "periodically",
+//         "unit_type" :"day"
+//     }
+// }
+(function(){
+    window.FictionalTime=new Object();
+    FictionalTime.getUnitVal=(
+        AM_TimeStamp    //Time Stamp Value in AM unit
+        ,calendar   // Fictional Time.Calendar 物件
+        ,unitName   // 單位名稱
+        ) => {
+         //將 TimeStamp轉為各種時間單位，ex: 月、年、小時
+
+         var curStamp=AM_TimeStamp;
+         var unitDefinition=calendar[unitName];
+         for(
+             var unitVal=0;
+             curStamp > unitDefinition.volume ;
+             ((curStamp-=unitDefinition.volume) & unitVal++) 
+         );
+
+         return {
+             "unit_name":unitName,
+             "unit_value" : unitVal
+         }
     }
-}
-
-function FictionalTime(){}
-
-FictionalTime.prototype.getValue=
-    function(request){
-        var start= new Date();
-        console.log("Start Time : " +start.getTime());
-        var unitDefinition=time_unit[request.unit_name];
-        var curStamp= request.timestamp;
-        for(
-            var unitVal=0;
-            curStamp > unitDefinition.volume ;
-            ((curStamp-=unitDefinition.volume) & unitVal++) 
-        );
-        var end = new Date();
-        
-        console.log("End Time : " +end.getTime() );
-        console.log("Elapsed Time : "+ (end.getTime()-start.getTime()));
-
-        return {
-            "unit_name":unitDefinition.name,
-            "unit_value" : unitVal
-        }
-    }
-
-//translate bigger unit to base unit
-FictionalTime.prototype.getAmVal=
-    function(unit_name,calendar){
+    FictionalTime.getUnitAmVal=(unitName,calendar)=>{
+        //取得各單位的AM值用來做計算
         var unitDef = calendar[unit_name];
         var amVal = unitDef.volume;
         var baseUnit = calendar[unitDef.unit_type];
@@ -73,8 +61,21 @@ FictionalTime.prototype.getAmVal=
             baseUnit = calendar[baseUnit.unit_type]; 
         }
         return amVal;
+        
     }
 
-window.ft=new FictionalTime();
+
+    //關於Calendar class的定義
+    FictionalTime.Calendar=function(){
+     function calendar(){};
+     
+     return calendar;      
+    
+    
+    }();
+
+
+
+}());
 
 
