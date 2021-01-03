e4e78c43fc8755832ca263f53b94371564119273
LowDensity <hailhailHaily@hotmail.com>
2021-01-03 23:42:18 +0800

    Can now grab from json. And has a unified control system.

--- FictionalTime.js
+++ FictionalTime.js
@@ -29,7 +29,9 @@
 //         "unit_type" :"day"
 //     }
 // }
+
 (function(){
+    if(typeof(window.FictionalTime)!=="undefined"){return console.log("duplicate initialization");} 
     window.FictionalTime=new Object();
     FictionalTime.getUnitVal=(
         AM_TimeStamp    //Time Stamp Value in AM unit
@@ -45,35 +47,97 @@
              curStamp > unitDefinition.volume ;
              ((curStamp-=unitDefinition.volume) & unitVal++) 
          );
-
          return {
              "unit_name":unitName,
              "unit_value" : unitVal
          }
     }
-    FictionalTime.getUnitAmVal=(unitName,calendar)=>{
+    FictionalTime.getUnitAmVal=(unitName,unit_list)=>{
         //取得各單位的AM值用來做計算
-        var unitDef = calendar[unitName];
+        var unitDef = unit_list[unitName];
         var amVal = unitDef.volume;
-        var baseUnit = calendar[unitDef.unit_type];
+        var baseUnit = unit_list[unitDef.unit_type];
+        var amBaseResolved=false;
+        console.log("aa");
         while(typeof(baseUnit)!="undefined"){
+            if(amBaseResolved){ console.log("not am");throw new Error("AM is not the only minimum unit of this Calendar.")}
             amVal = amVal*baseUnit.volume;
-            baseUnit = calendar[baseUnit.unit_type]; 
+            amBaseResolved = baseUnit.unit_type==="AM";
+            baseUnit = unit_list[baseUnit.unit_type]; 
         }
+        console.log("bb");
+        if(!amBaseResolved){throw new Error("Unable to resolve unit : " + unitName +" to AM(Absolute Minimum)");}
         return amVal;
         
     }
 
-    FictionalTime.Calendars =new Array();//已知的所有曆法，不確定會不會用到
-    FictionalTime.addCalendarFromBody=function(){
+    FictionalTime.Calendars =new Object();//已知的所有曆法，不確定會不會用到
+    FictionalTime.addCalendarFromBody=function(tag){
         //從script標籤的tag中取得曆法定義，主要用在啟動即載入的狀況
+        calendar_source=JSON.parse(tag.innerText);
+        if(typeof(calendar_source.calendar_name)=="undefined"
+         || calendar_source.calendar_name.trim()===""
+        ){throw new Error("A valid calendar Name is Required.");}
+
+        if(typeof(FictionalTime.Calendars.source[calendar_source.calendar_name])!="undefined")
+        {
+            console.warn("Duplicate Calendar Name for "+calendar_source.calendar_name);
+            return;
+        }
+
+        //keep source for later restoration and checking
+        FictionalTime.Calendars.source[calendar_source.calendar_name]=calendar_source;
+        var new_calendar= JSON.parse(tag.innerText);
+        //generating calendar unit_amval 
+        var errorUnits=new Array();
+
+        for(var i = 0 ; i< new_calendar.unit_list.length;i++){
+            try{
+                new_calendar.unit_list[i]["unit_amval"]=FictionalTime.getUnitAmVal(new_calendar.unit_list[i].unit_name,new_calendar.unit_list);
+            }catch(ex){
+                console.log(ex);
+                errorUnits.push(" unit_name = "+new_calendar.unit_list[i].unit_name);
+            }
+        }
+        console.log("eu.lngth "+errorUnits.length);
+        console.error( "Invalid calendar definition on unit : "+errorUnits.join(","));
+        if(errorUnits.length!=0){throw new Error( "Invalid calendar definition on unit : "+errorUnits.join(","));}
+        FictionalTime.Calendars.actual[new_calendar.calendar_name]=new_calendar;
     }
+    FictionalTime.Calendars={
+        "source" : new Object()
+        ,"actual" : new Object()
+    }
+    //scan the entire document for calendar definitions(script tag with class calendar_definition)
+    FictionalTime.showAlert=false;// if show alert for any initialization problem
+    FictionalTime.scanForCalendar=function(){
+        console.log("Scanning for calendar");
+        var errs = new Array();
+        var definitions=document.querySelectorAll("script.calendar_definition");
+        for(let i=0;i < definitions.length;i++){
+            let definition=definitions[i];
+            try{
+                FictionalTime.addCalendarFromBody(definition);
+            }catch(ex){
+                console.log(ex);
+            }
+        }
+        if(errs.length!=0){
+            if(FictionalTime.showAlert){
+                alert("Some Calendar Definition are invalid , see console for full information");
+            }
+            console.warn("Some Calendar Definition are invalid , see console for full information");
+        }
+        console.log("Calendar scan completed. Calendars Registered : ");
+        console.log(FictionalTime.Calendars);
+    }
+    
 
 
 
     //關於Calendar class的定義
     FictionalTime.Calendar=function(){
-     function calendar(){};
+     function calendar(){}
      //定義區塊，以下區塊定義這個物件的結構。
      var unit_list = new Object();
      var name = "Default Calendar" ; //這個曆法的名字，應該要從建構函式中接收
@@ -106,12 +170,11 @@
          for(var unitNo=0;unit_list[unitNo].unit_name!==unit_name.trim();unitNo++);
          return unit_list[unitNo];
      }
-     
      return calendar;      
     }();
+    // setup document initialization event.
+    window.addEventListener("load",function(ev){
+        FictionalTime.scanForCalendar(ev.document)});
 
-    /**載入後立即執行 */
-    FictionalTime.addCalendarFromBody();
 }());
-
 
